aca92d749a3c57f7d5bf55f8342096c6d4938d8f
myraushanraj <raushan10695@gmail.com>
2023-11-25 18:03:10 +0530

    done

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 12, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 1, 6, 0)}); // year, month, date, hour
         });
 });		
 
