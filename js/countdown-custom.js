b80a2cb7a110583b453831cc52b3c88d985af798
myraushanraj <raushan10695@gmail.com>
2023-11-27 12:39:48 +0530

    changes done

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,28 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 1, 6, 0)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 12, 6)}); // year, month, date, hour
         });
 });		
 
+
+
+// Counter To Count Number Visit
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  // Md.Asaduzzaman Muhid
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter').each(function() {
+        var $this = $(this);
+        jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+                $this.text(Math.ceil(this.Counter));
+            }
+        });
+    });
+    a = 1;// Md.Asaduzzaman Muhid
+  }
+});
